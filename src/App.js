38629260f2e385e5ab6deaf416454bf3048d7c22
Renzo Silva <renzoasilva@gmail.com>
2022-08-04 11:44:55 -0400

    my first react ever

--- src/App.js
+++ src/App.js
@@ -2,21 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let count = 0
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>My First React App</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          You click the button {count} time.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={() => {
+          console.log('I click the buttton')
+          count = count + 1
+        }}>Click Me</button>
       </header>
     </div>
   );
